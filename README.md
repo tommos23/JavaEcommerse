3cfbd04be20ec3a038f2695642f62eb1b62abedc
Michael Goddard <mikeygiom@gmail.com>
2014-03-17 19:01:44 +0000

    Updated code with working login, basic sessions and database connectivity

--- README.md
+++
@@ -1,4 +0,0 @@
-JavaEcommerse
-=============
-
-Java Files 
--- src/main/java/com/javaEcommerce/myapp/dao/ArticleDao.java
+++
@@ -1,8 +0,0 @@
-package com.javaEcommerce.myapp.dao;
-
-import java.util.List;
-import com.javaEcommerce.myapp.*;
-
-public interface ArticleDao {
-	public List<Article> getAllArticles();
-}
