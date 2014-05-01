1724232110e5be9b0687a3b584a12aa9e949978a
rohithate <rmhate1@sheffield.ac.uk>
2014-05-01 19:45:21 +0100

    added emailservice class to send email

---
+++ VMApp1/src/EmailService.java
@@ -0,0 +1,48 @@
+
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class EmailService {	
+
+	public void sendEmail(String toemail, String subject, String textmessage){
+		final String username = "oj.noreply@gmail.com";
+		final String password = "journal2journal";
+
+		Properties props = new Properties();
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.smtp.host", "smtp.gmail.com");
+		props.put("mail.smtp.port", "587");
+
+		Session session = Session.getInstance(props,
+				new javax.mail.Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(username, password);
+			}
+		});
+
+		try {
+
+			Message message = new MimeMessage(session);
+			message.setFrom(new InternetAddress("oj.noreply@gmail.com"));
+			message.setRecipients(Message.RecipientType.TO,
+					InternetAddress.parse(toemail));
+			message.setSubject(subject);
+			message.setText(textmessage);
+			
+			Transport.send(message);
+
+			System.out.println("Sent Email to "+toemail);
+
+		} catch (MessagingException e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
