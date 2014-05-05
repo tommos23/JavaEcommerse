2ff407b7184dab3845b59e004751807df7a32d5f
Aaron Thompson <tommos23@gmail.com>
2014-05-05 22:04:58 +0100

    update review model and table

--- VMApp1/Models/com/jg/Model/Review.java
+++ VMApp1/Models/com/jg/Model/Review.java
@@ -33,6 +33,12 @@ public class Review {
 	public void setArticle(Article article) {
 		this.article = article;
 	}
+	public Version getVersion() {
+		return version;
+	}
+	public void setVerion(Version version) {
+		this.version = version;
+	}
 	public int getPosition() {
 		return position;
 	}
@@ -76,12 +82,14 @@ public class Review {
 	private User reviewer;//int reviewer_id;
 	@ManyToOne(cascade = CascadeType.ALL)
 	private Article article;//int article_id;
+	@ManyToOne(cascade = CascadeType.ALL)
+	private Version version;
 	private int position;
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date created_at;
 	@Column(columnDefinition = "TEXT")
 	private String contribution;
-	@Column(columnDefinition = "TEXT")
+	@Column(columnDefinition = "TEXT")	
 	private String critism;
 	private int expertise;
 	private int status;
