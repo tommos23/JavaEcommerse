28928d3e3d695853874f13aa6b2f3beb6e19253b
Michael Goddard <mikeygiom@gmail.com>
2014-05-06 14:28:18 +0100

    Added create letter and reply to letter. Removed Aaron's bloody documentation

---
+++ VMApp1/LettersToEditors/com/jg/ViewServlets/EditedLetters.java
@@ -0,0 +1,94 @@
+package com.jg.ViewServlets;
+
+import java.io.IOException;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.apache.velocity.Template;
+import org.apache.velocity.context.Context;
+import org.apache.velocity.tools.view.VelocityViewServlet;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.SharedSessionContract;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import com.jg.Controller.LettersToEditorsController;
+import com.jg.Controller.UserController;
+import com.jg.Model.*;
+
+/**
+ * Servlet implementation class Uploads
+ */
+public class EditedLetters extends VelocityViewServlet {
+	private static final long serialVersionUID = 1L;
+
+	public Template handleRequest( HttpServletRequest request, 
+			HttpServletResponse response, Context context )
+	{
+		HttpSession session = request.getSession(true);
+		session.setMaxInactiveInterval(30*60);
+		if (session.isNew()){
+			session.setAttribute("user", "false");
+			System.out.println("false");
+		}
+		else if(session.getAttribute("user") == null){
+				session.setAttribute("user", "false");
+		}
+		//------Code to display alert message------
+		if(session.getAttribute("alertMessage")!=null){
+			context.put("alertMessage",session.getAttribute("alertMessage").toString());
+			context.put("showAlert", "true");
+			if(session.getAttribute("alertType") != null)
+				context.put("alertType",session.getAttribute("alertType").toString());
+			else
+				context.put("alertType", "info");
+			session.setAttribute("alertMessage", null);
+			session.setAttribute("alertType", null);
+		}
+		//-----End of Alert Message Code---------
+		UserController uc = new UserController();
+		uc.startSession();
+		User thisUser = null;
+		if (session.getAttribute("user_id") != null) {
+			thisUser = uc.get(Integer.parseInt(session.getAttribute("user_id").toString()));
+		}
+		if (thisUser != null && (thisUser.getRole().getName().equals("activeauthor") || thisUser.getRole().getName().equals("passiveauthor"))) {
+			try {
+				LettersToEditorsController ltec = new LettersToEditorsController();
+				ltec.startSession();
+				context.put("letters", ltec.getAllLettersToEditors(1));
+				context.put("thisuser", uc.get(Integer.parseInt(session.getAttribute("user_id").toString())));
+				
+				ltec.endSession();
+			} catch(Exception e ) {
+				System.out.println("Error " + e);
+			}
+		}
+		else {
+			if (uc.isSessionReady())
+				uc.endSession();
+			String alertMessage = "<Strong>Oops!!</strong> You do not have permission to do that.";
+			String alertType = "danger";
+			session.setAttribute("alertMessage",alertMessage);
+			session.setAttribute("alertType",alertType );
+			try {
+				response.sendRedirect("welcome");
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		/* get the template */
+		Template template = null;
+		template = getTemplate("letters/editedletters.vm"); 
+		return template;
+	}
+
+}
